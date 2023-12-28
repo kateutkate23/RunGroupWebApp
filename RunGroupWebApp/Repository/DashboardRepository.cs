6a7dc012b3e20d2a578ed8525c969500d89bdeba
kate <kate_utkate@vk.com>
2023-12-28 11:14:40 +0300

    initial

---
+++ RunGroupWebApp/Repository/DashboardRepository.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RunGroupWebApp.Data;
+using RunGroupWebApp.Interfaces;
+using RunGroupWebApp.Models;
+using RunGroupWebApp.ViewModels;
+
+namespace RunGroupWebApp.Repository
+{
+    public class DashboardRepository : IDashboardRepository
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public DashboardRepository(ApplicationDbContext context, IHttpContextAccessor httpContextAccessor)
+        {
+            _context = context;
+            _httpContextAccessor = httpContextAccessor;
+        }
+        public async Task<List<Club>> GetAllUserClubs()
+        {
+            var currentUser = _httpContextAccessor.HttpContext?.User.GetUserId();
+            var userClubs = _context.Clubs.Where(r => r.AppUser.Id == currentUser);
+
+            return userClubs.ToList();
+        }
+
+        public async Task<List<Race>> GetAllUserRaces()
+        {
+            var currentUser = _httpContextAccessor.HttpContext?.User.GetUserId();
+            var userRaces = _context.Races.Where(r => r.AppUser.Id == currentUser);
+
+            return userRaces.ToList();
+        }
+
+        public async Task<AppUser> GetUserById(string id)
+        {
+            return await _context.Users.FindAsync(id);
+        }
+
+        public async Task<AppUser> GetUserByIdNoTracking(string id)
+        {
+            return await _context.Users.Where(u => u.Id == id).AsNoTracking().FirstOrDefaultAsync();
+        }
+
+        public bool Update(AppUser user)
+        {
+            _context.Users.Update(user);
+            return Save();
+        }
+
+        public bool Save()
+        {
+            var saved = _context.SaveChanges();
+            return saved > 0;
+        }
+    }
+}
